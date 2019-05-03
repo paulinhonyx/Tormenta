2cab6ba495ab37da7ecbb8a7aa4f8afac6e0c2dc
Paulinhonyx <paulinhonyx@hotmail.com>
2019-05-03 14:37:27 -0300

    att

--- src/pages/primeiraTela.js
+++ src/pages/primeiraTela.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View,} from "react-native";
 import { Appbar, } from "react-native-paper";
 
 import styles from "./styles";
