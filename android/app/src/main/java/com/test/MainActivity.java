d24fff4acce6de771a5255f0aabb2f300c5fd13d
paulinhonyx <34190795+paulinhonyx@users.noreply.github.com>
2019-05-17 20:35:46 -0300

    Update MainActivity.java

--- android/app/src/main/java/com/test/MainActivity.java
+++ android/app/src/main/java/com/test/MainActivity.java
@@ -18,10 +18,10 @@ public class MainActivity extends ReactActivity {
     }
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+            return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
     };
   }
