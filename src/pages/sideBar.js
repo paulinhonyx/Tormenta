fde0e1da557984e5db362e19d3d7364fe2f0a884
Paulinhonyx <paulinhonyx@hotmail.com>
2019-05-25 16:30:23 -0300

    Drawer

--- src/pages/sideBar.js
+++ src/pages/sideBar.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
-import { Text } from "react-native-paper";
+import { Text, List, Drawer } from "react-native-paper";
+
+import styles from "../config/styles";
+const routes = ["Home", "Perfil"];
 
 export default class SideBar extends Component {
   render() {
-    return <Text>Hello</Text>;
+    return (
+      <Drawer.Section style={styles.drawer}>
+        <Drawer.Item
+          label="First Item"
+          onPress={() => this.props.navigation.navigate("Perfil")}
+        />
+        <Drawer.Item
+          label="Second Item"
+          onPress={() => this.props.navigation.navigate("Home")}
+        />
+      </Drawer.Section>
+    );
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -6,38 +6,48 @@ import {
 } from "react-navigation";
 import { Provider as PaperProvider } from "react-native-paper";
 
+import SideBar from "./pages/sideBar";
+
+import Home from "./pages/home";
 import SingIn from "./pages/SingIn";
 import Cadastro from "./pages/cadastrar";
-import Home from "./pages/home";
 import Perfil from "./pages/perfil";
-import criarMesa from "./pages/criarMesa";
-import procurarMesa from "./pages/procurarMesa";
-import drawerMenu from "./pages/drawerMenu";
-import Pericias from "./pages/pericias";
 
-const Routes = createStackNavigator({
-  SingIn,
-  Cadastro,
-  Home,
-  Perfil,
-  criarMesa,
-  procurarMesa,
-  drawerMenu,
-  Pericias
-});
+const DrawerNavigator = createDrawerNavigator(
+  {
+    Home: { screen: Home },
+    Perfil: { screen: Perfil }
+  },
+  {
+    initialRouteName: "Home",
+    drawerWidth: 300,
+    contentComponent: props => <SideBar {...props} />
+  }
+);
 
-const DrawerNavigator = createDrawerNavigator({
-  SingIn,
-  Cadastro,
-  Home,
-  Perfil,
-  criarMesa,
-  procurarMesa,
-  drawerMenu,
-  Pericias
-});
+const HomeStack = createStackNavigator(
+  {
+    Home: { screen: DrawerNavigator }
+  },
+  {
+    initialRouteName: "Home",
+    headerMode: "none"
+  }
+);
 
-const App = createAppContainer(Routes, DrawerNavigator);
+const Routes = createStackNavigator(
+  {
+    SingIn: { screen: SingIn },
+    Home: { screen: DrawerNavigator },
+    Cadastro: { screen: Cadastro }
+  },
+  {
+    initialRouteName: "SingIn",
+    headerMode: "none"
+  }
+);
+
+const App = createAppContainer(Routes);
 
 export default () => {
   return (
