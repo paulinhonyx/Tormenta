5abc20742b1c863a20ddf58935332780c676ec20
Paulinhonyx <paulinhonyx@hotmail.com>
2019-05-25 18:08:09 -0300

    Import SearchBar

--- src/pages/procurarMesa.js
+++ src/pages/procurarMesa.js
@@ -6,7 +6,7 @@ import {
   FlatList,
   TouchableOpacity
 } from "react-native";
-import { Appbar, ActivityIndicator } from "react-native-paper";
+import { Appbar, ActivityIndicator, Searchbar } from "react-native-paper";
 
 import styles from "../config/styles";
 import Cards from "../components/Cards";
@@ -24,17 +24,56 @@ export default class procurarMesa extends Component {
 
   componentDidMount() {
     this._get("http://5ce16d028ad3c700145b7c26.mockapi.io/mesas").then(data => {
-      this.setState({ items: data });
+      this.setState({ data: data, dataSource: data });
     });
   }
 
   constructor() {
     super();
-    this.state = { nomeCodigo: "", items: [] };
+    this.state = { loading: false, data: [], value: "" };
   }
 
+  searchFilterFunction = text => {
+    this.setState({
+      value: text
+    });
+
+    const newData = this.state.data.filter(item => {
+      const itemData = `${item.title.toUpperCase()} ${item.id.toUpperCase()}`;
+      const textData = text.toUpperCase();
+      return itemData.includes(textData);
+    });
+
+    this.setState({
+      dataSource: newData
+    });
+  };
+
+  renderHeader = () => {
+    return (
+      <Searchbar
+        placeholder="Type..."
+        value={this.state.value}
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+      />
+    );
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#CED0CE"
+        }}
+      />
+    );
+  };
+
   render() {
-    if (this.state.items.length === 0) {
+    if (this.state.data.length === 0) {
       return (
         <View>
           <ActivityIndicator size="large" />
@@ -42,25 +81,38 @@ export default class procurarMesa extends Component {
       );
     }
 
-    return (
-      <ScrollView>
-        <Text style={styles.nomeCodigoText}>Mesas:</Text>
-
-        <View style={styles.procurarCard}>
-          <FlatList
-            data={this.state.items}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                onPress={() => this.props.navigation.push("Pericias")}
-              >
-                <Cards item={item} />
-              </TouchableOpacity>
-            )}
-          />
+    if (this.state.loading) {
+      return (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <ActivityIndicator />
         </View>
-      </ScrollView>
-    );
+      );
+    } else {
+      return (
+        <ScrollView>
+          <Text style={styles.nomeCodigoText}>Mesas:</Text>
+
+          <View style={styles.procurarCard}>
+            <FlatList
+              data={this.state.dataSource}
+              keyExtractor={(item, index) => index.toString()}
+              extraData={this.state}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.push("Pericias")}
+                >
+                  <Cards item={item} />
+                </TouchableOpacity>
+              )}
+              ItemSeparatorComponent={this.renderSeparator}
+              ListHeaderComponent={this.renderHeader}
+            />
+          </View>
+        </ScrollView>
+      );
+    }
   }
   _get = async endpoint => {
     const res = await fetch(endpoint);
