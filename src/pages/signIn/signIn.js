aa03649d6408f5c604aa489e15f8d0ce8d9e07b6
Paulinhonyx <paulinhonyx@hotmail.com>
2019-06-04 20:07:42 -0300

    login

--- src/pages/signIn/signIn.js
+++ src/pages/signIn/signIn.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { Image, View } from "react-native";
+import { Image, View, Text } from "react-native";
 import { TextInput, Button } from "react-native-paper";
+
+import Axios from "axios";
+import PropTypes from "prop-types";
 
 import styles from "../../config/styles";
 
@@ -10,8 +13,42 @@ export default class SignIn extends Component {
   });
 
   state = {
-    usuario: "",
-    senha: ""
+    usuario: "katchaca",
+    senha: "12345678",
+    error: ""
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+      dispatch: PropTypes.func
+    }).isRequired
+  };
+
+  handleSignInPress = async () => {
+    if (this.state.usuario.length === 0 || this.state.senha.length === 0) {
+      this.setState(
+        { error: "Preencha usuário e senha para continuar!" },
+        () => false
+      );
+    } else {
+      try {
+        const response = await Axios.post(
+          "http://5ce16d028ad3c700145b7c26.mockapi.io/users",
+          {
+            nome: this.state.usuario,
+            senha: this.state.senha
+          }
+        );
+
+        console.log(response);
+        this.props.navigation.navigate("Home");
+      } catch (_err) {
+        this.setState({
+          error: "Houve um problema com o login, verifique suas credenciais!"
+        });
+      }
+    }
   };
 
   render() {
@@ -26,18 +63,22 @@ export default class SignIn extends Component {
           style={styles.textContainer}
           value={this.state.usuario}
           onChangeText={usuario => this.setState({ usuario })}
+          autoCapitalize="none"
+          autoCorrect={false}
         />
         <TextInput
           label="Senha"
           style={styles.textContainer}
           value={this.state.senha}
           onChangeText={senha => this.setState({ senha })}
+          autoCorrect={false}
           secureTextEntry={true}
         />
+        {this.state.error.length !== 0 && <Text>{this.state.error}</Text>}
         <Button
           style={styles.entrar}
           mode="contained"
-          onPress={() => this.props.navigation.navigate("Home")}
+          onPress={this.handleSignInPress}
         >
           Entrar
         </Button>
